6e8b99cd5fe72b68e5db338153a3db623cb58514
Garrett Pfingston <gpfingston@gmail.com>
2025-12-09 14:35:11 -0600

    cleaned up TODO statements

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Welcome to the Products API!", "resource_endpoint": "/products"}), 200
 
-# TODO: Implement GET /products route that returns all products or filters by category
-
 @app.route("/products", methods=["GET"])
 def get_products():
     category = request.args.get("category")
@@ -16,8 +14,6 @@ def get_products():
         filtered_products = [p for p in products if p["category"].lower() == category.lower()]
         return jsonify(filtered_products), 200
     return jsonify(products), 200
-
-# TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
 @app.route("/products/<int:id>", methods=["GET"])
 def get_product_by_id(id):
